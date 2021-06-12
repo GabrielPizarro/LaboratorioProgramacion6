1df54b2e4812014b380c93eb520d28ddaae5e253
Gabriel Pizarro <jgapizarro@gmail.com>
2021-06-11 20:42:42 -0600

    Page, Grid

---
+++ WebApp/Pages/MarcaVehiculo/Grid.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.MarcaVehiculo
+{
+    public class GridModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        
+
+        public void OnDelete()
+        {
+        }
+    }
+}
